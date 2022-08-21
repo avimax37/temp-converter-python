34fd8f52d18bd53033457af07eea934e433dce57
Avinaba Bera <avimax37@gmail.com>
2022-08-21 11:28:48 +0530

    Update

--- tempConverter.py
+++ tempConverter.py
@@ -9,32 +9,32 @@ choice = int(input())
 
 match choice:
     case 1:
-        print("Enter temperature in C for conversion:")
+        print("Enter temperature in C for conversion:", end=" ")
         temp = float(input())
         newTemp = temp * (9 / 5) + 32
         print("Temperature is:", newTemp, "F")
     case 2:
-        print("Enter temperature in F for conversion:")
+        print("Enter temperature in F for conversion:", end=" ")
         temp = float(input())
         newTemp = (temp - 32) * (5 / 9)
         print("Temperature is:", newTemp, "C")
     case 3:
-        print("Enter temperature in C for conversion:")
+        print("Enter temperature in C for conversion:", end=" ")
         temp = float(input())
         newTemp = temp + 273.15
         print("Temperature is:", newTemp, "K")
     case 4:
-        print("Enter temperature in K for conversion:")
+        print("Enter temperature in K for conversion:", end=" ")
         temp = float(input())
         newTemp = temp - 273.15
         print("Temperature is:", newTemp, "C")
     case 5:
-        print("Enter temperature in F for conversion:")
+        print("Enter temperature in F for conversion:", end=" ")
         temp = float(input())
         newTemp = (temp - 32) * (5 / 9) + 273.15
         print("Temperature is:", newTemp, "K")
     case 6:
-        print("Enter temperature in K for conversion:")
+        print("Enter temperature in K for conversion:", end=" ")
         temp = float(input())
         newTemp = (temp - 273.15) * (9 / 5) + 32
         print("Temperature is:", newTemp, "F")
